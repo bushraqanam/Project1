542b49cbd92e9b515b6af53b5cf44604bbe718e3
bushraqanam <bqanam@gmail.com>
2024-08-06 14:39:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,196 @@
+//operators//
+//arthimatic operators// addition,subraction,multiply.division,exponentation,modulus
+//unary operators,1 increament 2 decreament 
+//combining operators//
+//assignment operators//
+//comparison operators//
+//logical operators//
+//addition//
+var num1 = 45;
+var num2 = 25;
+var num3 = num1 / num2;
+console.log(num3);
+console.log(num3);
+console.log(num3);
+var sum = 35 + 45;
+console.log(sum);
+console.log(25 * 35 * 22 * 17 * 31 * 19);
+//addition//
+var num4 = 35;
+var num5 = 35;
+var num6 = num4 + num5;
+console.log(num6);
+console.log(num6);
+num4 = 25;
+console.log(num4);
+num5 = 65;
+console.log(num5);
+var num7 = num4 + num5;
+console.log(num7);
+var num8 = 85;
+var num9 = 55;
+var num10 = num8 + num9;
+console.log(num10);
+//subraction//
+var num11 = 75;
+var num12 = 25;
+var num13 = num11 - num12;
+console.log(num13);
+num11 = 45 - 12;
+console.log(num11);
+num12 = 65 - 20;
+console.log(num12);
+var num14 = 95;
+var num15 = 55;
+var num16 = num14 - num15;
+console.log(num16);
+var sum1 = 88 - 25;
+console.log(sum1);
+console.log(92 - 48, 77 - 12, 33 - 14);
+console.log(76 - 32, 55 - 18, 87 - 29);
+//multiply//
+var num17 = 40;
+var num18 = 22;
+var num19 = num17 * num18;
+console.log(num19);
+num17 = 20 * 5;
+console.log(num17);
+num18 = 45 * 8;
+console.log(num18);
+num19 = 7 * 9;
+console.log(num19);
+console.log(20 * 5, 45 * 4, 30 * 2);
+num18 = 30 * 4;
+console.log(num18);
+num19 = 50 * 8;
+console.log(num19);
+//division//
+var num20 = 75;
+var num21 = 35;
+var num22 = num20 / num21;
+console.log(num22);
+var num23 = 45;
+var num24 = 27;
+var num25 = num23 / num24;
+console.log(num25);
+num20 = 34 / 28;
+console.log(num20);
+num21 = 43 / 38;
+console.log(num21);
+num23 = 12 / 2;
+console.log(num23);
+num24 = 40 / 4;
+console.log(num24);
+num25 = 95 / 6;
+console.log(num25);
+num22 = 50 / 2;
+console.log(num22);
+console.log(10 / 2, 35 / 4, 65 / 4, 20 / 2);
+var num26 = 67 / 3;
+console.log(num26);
+var num27 = 85;
+var num28 = 2;
+var num29 = num27 / num28;
+console.log(num29);
+num27 = 75 / 3;
+console.log(num27);
+num28 = 100 / 2;
+console.log(num28);
+num29 = 250 / 2;
+console.log(num29);
+//exponentation,modulus//
+var A = 5;
+var power = 4;
+var result = Math.pow(A, power);
+console.log(result);
+var num30 = Math.pow(2, 3);
+console.log(num30);
+//modules//
+var mood = 59 % 3;
+console.log(mood);
+//unary operators//
+//increment//dicerment//
+var abc = 25;
+console.log(++abc);
+console.log(abc++);
+console.log(abc);
+var max = 35;
+--max;
+console.log(max);
+console.log(max--);
+console.log(max);
+var num31 = 8;
+var num32 = 10;
+var result1 = num31++ - --num32 - num31++ - --num32 - ++num32;
+//8+1=9 , 10-1=9 , 9+1=10 , 9-1=8 , 8+1=9
+//9-9-10-8-9=
+//1//
+console.log(result1);
+var count = 0;
+++count;
+console.log(count);
+++count;
+console.log(count);
+++count;
+console.log(count);
+--count;
+console.log(count);
+--count;
+console.log(count);
+//2//
+var num33 = 4;
+--num33;
+console.log(num33);
+--num33;
+console.log(num33);
+--num33;
+console.log(num33);
+--num33;
+console.log(num33);
+++num33;
+console.log(num33);
+++num33;
+console.log(num33);
+++num33;
+console.log(num33);
+++num33;
+console.log(num33);
+//3//
+var a = 8;
+var b = 4;
+var sum2 = a + b;
+console.log(sum2);
+console.log(Math.pow(a, b));
+var c = 8;
+var d = 4;
+var result2 = Math.pow(c, d);
+console.log(result2);
+var mood1 = 8 % 4;
+console.log(mood1);
+//4//
+var x = 10 + 5 * 3 - 8 / 2 % 3;
+console.log(x);
+var N = 5;
+++N;
+console.log(N);
+5 * 2;
+console.log(N);
+N = 5 * 2;
+console.log(N);
+--N;
+console.log(N);
+--N;
+console.log(N);
+--N;
+console.log(N);
+var sum3 = 9;
+var sum4 = 5;
+var result3 = ++sum3 + --sum4 + sum4++ + --sum3 + --sum4 + ++sum3;
+/////////////9+1=10 , 5-1=4, 4+1=5 , 10-1=9, 5-1=4 , 9+1=10
+console.log(result3);
+var sum5 = 6;
+var sum6 = 10;
+var result4 = ++sum5 - --sum6 - sum5++ - sum6-- - ++sum5 - --sum6;
+//////////////6+1=7 , 10-1=9 , 7+1=8 , 9-1=8 , 8+1=9 , 8-1=7
+////////////// 7-9-8-8-9-7=
+console.log(result4);
---
+++ main.ts
@@ -0,0 +1,215 @@
+//operators//
+//arthimatic operators// addition,subraction,multiply.division,exponentation,modulus
+//unary operators,1 increament 2 decreament 
+//combining operators//
+//assignment operators//
+//comparison operators//
+//logical operators//
+
+
+//addition//
+let num1:number = 45
+let num2:number = 25
+let num3:number = num1 / num2
+console .log(num3)
+console.log(num3)
+console.log(num3)
+let sum:number = 35+45
+console.log(sum)
+console.log(25*35*22*17*31*19)
+//addition//
+let num4:number = 35
+let num5:number = 35
+let num6:number = num4 + num5
+console.log(num6)
+console.log(num6)
+num4 = 25
+console.log(num4)
+num5 = 65
+console.log(num5)
+let num7:number = num4 + num5
+console.log(num7)
+let num8:number = 85
+let num9:number = 55
+let num10:number = num8 + num9
+console.log(num10)
+//subraction//
+let num11:number = 75
+let num12:number = 25
+let num13:number = num11 - num12
+console.log(num13)
+num11 = 45 - 12
+console.log(num11)
+num12 = 65 - 20
+console.log(num12)
+let num14:number = 95
+let num15:number = 55
+let num16:number = num14 - num15
+console.log(num16)
+let sum1:number = 88-25
+console.log(sum1)
+console.log(92-48,77-12,33-14)
+console.log(76-32,55-18,87-29)
+//multiply//
+let num17:number = 40
+let num18:number = 22
+let num19:number = num17 * num18
+console.log(num19)
+num17 = 20 * 5
+console.log(num17)
+num18 = 45 * 8
+console.log(num18)
+num19 = 7 * 9
+console.log(num19)
+console.log(20*5,45*4,30*2)
+num18 = 30 * 4
+console.log(num18)
+num19 = 50 * 8
+console.log(num19)
+//division//
+let num20 = 75
+let num21 = 35
+let num22:number = num20 / num21
+console.log(num22)
+let num23 = 45
+let num24 = 27
+let num25:number = num23 / num24
+console.log(num25)
+num20 = 34 / 28
+console.log(num20)
+num21 = 43 / 38
+console.log(num21)
+num23 = 12 / 2
+console.log(num23)
+num24 = 40 / 4
+console.log(num24)
+num25 = 95 / 6
+console.log(num25)
+num22 = 50 / 2
+console.log(num22)
+console.log(10/2,35/4,65/4,20/2)
+let num26 = 67 / 3
+console.log(num26)
+let num27 = 85
+let num28 = 2
+let num29 = num27 / num28
+console.log(num29)
+num27 = 75 / 3
+console.log(num27) 
+num28 = 100 / 2
+console.log(num28)
+num29 = 250 / 2
+console.log(num29)
+//exponentation,modulus//
+let A:number = 5
+let power:number = 4
+let result:number = A**power
+console.log(result)
+let num30:number = 2 ** 3
+console.log(num30)
+//modules//
+let mood:number = 59%3
+console.log(mood)
+//unary operators//
+//increment//dicerment//
+let abc:number = 25
+
+console.log(++abc)
+
+console.log(abc++)
+console.log(abc)
+let max:number = 35
+--max
+console.log(max)
+
+console.log(max--)
+console.log(max)
+let num31:number = 8
+let num32:number = 10
+ let result1 = num31++ - --num32 - num31++ - --num32 - ++num32
+ //8+1=9 , 10-1=9 , 9+1=10 , 9-1=8 , 8+1=9
+ //9-9-10-8-9=
+ //1//
+ console.log(result1)
+ let count:number = 0
+ ++count
+ console.log(count)
+ ++count
+ console.log(count)
+ ++count
+ console.log(count)
+ --count
+ console.log(count)
+ --count
+ console.log(count)
+//2//
+let num33:number = 4
+--num33
+console.log(num33)
+--num33
+console.log(num33)
+--num33
+console.log(num33)
+--num33
+console.log(num33)
+++num33
+console.log(num33)
+++num33
+console.log(num33)
+++num33
+console.log(num33)
+++num33
+console.log(num33)
+//3//
+let a:number = 8
+let b:number = 4
+let sum2 = a+b
+console.log(sum2)
+console.log(a**b)
+let c:number = 8
+let d:number = 4
+let result2:number = c**d
+console.log(result2)
+let mood1:number = 8%4
+console.log(mood1)
+//4//
+let x:number = 10+5*3-8/2%3
+console.log(x)
+let N:number = 5
+++N
+console.log(N)
+5*2
+console.log(N)
+N = 5*2
+console.log(N)
+--N
+console.log(N)
+--N
+console.log(N)
+--N
+console.log(N)
+let sum3:number = 9
+let sum4:number = 5
+let result3 = ++sum3 + --sum4 + sum4++ + --sum3 + --sum4 + ++sum3
+/////////////9+1=10 , 5-1=4, 4+1=5 , 10-1=9, 5-1=4 , 9+1=10
+console.log(result3)
+let sum5:number = 6
+let sum6:number = 10
+let result4 = ++sum5 - --sum6 - sum5++ - sum6-- - ++sum5 - --sum6
+//////////////6+1=7 , 10-1=9 , 7+1=8 , 9-1=8 , 8+1=9 , 8-1=7
+////////////// 7-9-8-8-9-7=
+console.log(result4)
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
